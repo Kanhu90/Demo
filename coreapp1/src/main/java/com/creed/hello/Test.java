92e4304be80a869c529572c635bf5820ff4cb3a5
kanhu90 <kanhu90@gmail.com>
2016-06-29 05:00:36 +0530

    static block is added

--- coreapp1/src/main/java/com/creed/hello/Test.java
+++ coreapp1/src/main/java/com/creed/hello/Test.java
@@ -1,6 +1,9 @@
 package com.creed.hello;
 
 public class Test {
+	static {
+		System.out.println("Test-S.B");
+	}
  public static void main(String args[]) {
 	 System.out.println("-----------------");
 	 System.out.println("Hello Guys");
