094df84e140ed48b702fa0fd79685d6d628b61d6
kanhu90 <kanhu90@gmail.com>
2016-06-29 05:42:48 +0530

    added

---
+++ coreapp1/src/main/java/com/creed/hello/Test.java
@@ -0,0 +1,16 @@
+package com.creed.hello;
+
+public class Test {
+	static {
+		System.out.println("Test-S.B");
+	}
+ public static void main(String args[]) {
+	 System.out.println("-----------------");
+	 System.out.println("Hello Guys");
+	 System.out.println("Command Line Args");
+	 for (String string : args) {
+		System.out.println(string);
+	}
+	System.out.println("-------------------");
+}
+}
